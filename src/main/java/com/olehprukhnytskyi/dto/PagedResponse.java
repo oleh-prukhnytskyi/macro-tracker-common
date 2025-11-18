90867a254cb9b8b639ba2cc8eeca2764ed5d1d35
Oleg Pruh <olegpruh1@gmail.com>
2025-11-18 18:57:39 +0000

    Add constructors to dtos

--- src/main/java/com/olehprukhnytskyi/dto/PagedResponse.java
+++ src/main/java/com/olehprukhnytskyi/dto/PagedResponse.java
@@ -2,10 +2,15 @@ package com.olehprukhnytskyi.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Schema(description = "Paginated response wrapper")
 public class PagedResponse<T> {
--- src/main/java/com/olehprukhnytskyi/dto/Pagination.java
+++ src/main/java/com/olehprukhnytskyi/dto/Pagination.java
@@ -2,9 +2,13 @@ package com.olehprukhnytskyi.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Schema(description = "Pagination metadata")
 public class Pagination {
